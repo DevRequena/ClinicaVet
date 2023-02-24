cb3faf278cee22f1fc0911745eba5c16c37e98c5
DevRequena <devcesarperez@gmail.com>
2023-02-24 10:43:19 -0400

    listooo

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+
+export const Error = () => {
+  return (  
+      <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-lg'>
+          <p>Todos los Campos son Obligatorios</p>
+      </div>
+    
+  )
+}
